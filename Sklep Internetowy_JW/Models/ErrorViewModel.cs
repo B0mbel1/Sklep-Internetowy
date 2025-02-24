877b87ae1841d215aa76f3e349dc461d9b65d429
112 <112@112-14>
2025-02-24 15:43:17 +0100

    Dodaj pliki projektów.

---
+++ Sklep Internetowy_JW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sklep_Internetowy_JW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
